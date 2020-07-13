199bd5a25d969d3d8f04f2c469942d0b6676ed1e
Travis F. Collins <travis.collins@analog.com>
2020-07-12 23:43:43 -0400

    Update docker-entrypoint-hdl.sh

--- docker-entrypoint-hdl.sh
+++ docker-entrypoint-hdl.sh
@@ -3,12 +3,14 @@ set -e
 
 # Copy read-only folder(s) into readable folder(s)
 if [ -d "/root/.Xilinxro" ]; then
+  echo "Adding Xilinx dot files"
   cp -r /root/.Xilinxro /root/.Xilinx
 else
   echo ".Xilinxro folder does not exist"
 fi
 
 if [ -d "/root/.matlabro" ]; then
+  echo "Adding MATLAB dot files"
   cp -r /root/.matlabro /root/.matlab
 else
   echo ".matlabro folder does not exist"
